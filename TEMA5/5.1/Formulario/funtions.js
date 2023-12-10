5e9fae390f5bbc70545e99c75e7963c58e649342
Alberto Calzada González-Román <alberto.calzada@educa.madrid.org>
2023-12-10 17:26:07 +0100

    creando cookie

--- TEMA5/5.1/Formulario/funtions.js
+++ TEMA5/5.1/Formulario/funtions.js
@@ -1,6 +1,6 @@
 var miv;
 function AbrirVentana() {
-    miv = window.open("catalogo.html","v2","top=150,left=400,width=200");
+    miv = window.open("catalogo.html", "v2", "top=150,left=400,width=200");
 }
 function Calcula() {
     var lacant = document.getElementById("cant").value;
@@ -20,7 +20,7 @@ function Cerrar() {
     var Suma = 0;
     var el_form = document.getElementById('prods');
     for (let i = 0; i < el_form.elements.length; i++) {
-        if (el_form.elements[i].type=='checkbox' && el_form.elements[i].checked == true) {
+        if (el_form.elements[i].type == 'checkbox' && el_form.elements[i].checked == true) {
             Suma = Suma + parseInt(el_form.elements[i].value);
         }
         window.opener.document.getElementById('cant').value = Suma;
